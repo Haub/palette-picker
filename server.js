e2269c034be9278e639ec8c2740f1800853925ee
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-27 13:47:43 -0700

    Add initial setup for express in server.js

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.get('/', (request, response) => {
-  response.send('hello world');
+app.get('/', (response, request) => {
+  response.send('Welcome to Palette Picker')
+})
+
+app.get('/api/v1/projects', (request, response) => {
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  
+});
+
+app.post('/api/v1/projects', (request, response) => {
+});
+
+app.post('api/v1/palettes', (request, response) => {
+
 });
 
 app.listen(3000, () => {
-  console.log('Express intro running on localhost:3000');
+  console.log(`${app.locals.title} is running on ${app.get('port')}`);
 });
