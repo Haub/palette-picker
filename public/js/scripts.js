cfe6f7ef4c535e781d4b8950042e9e5ba2299a65
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-27 20:21:27 -0700

    Refactor createPalette with a map and add hex text color to each circle

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,20 +1,29 @@
 
-const generateColors = document.querySelector('.generate-colors');
+const generateColorsButton = document.querySelector('.generate-colors');
 
 const colors = document.querySelectorAll('.color-button');
+const hexColors = document.querySelectorAll('h5');
 
-window.addEventListener('load', () => {
-  createPalette();
-})
+window.addEventListener('load', () => { createPalette() });
+generateColorsButton.addEventListener('click', () => { generateNewPalette() });
 
 const createColor = () => {
   return '#' + ('000000' + Math.floor(16777216 * Math.random()).toString(16)).slice(-6);
 }
 
+let newColorArray = [];
+
 const createPalette = () => {
+  const newColors = [...colors].map(color => {
+    let newColor = createColor();
+    color.style.backgroundColor = newColor;
+    newColorArray.push(newColor);
+  })
+  newColorArray.map((hexColor, index) => hexColors[index].innerText = hexColor.toString());
   
-  for(i=0; i<colors.length; i++) {
-    colors[i].style.backgroundColor = createColor();
-  }
-  return;
+  return newColors;
+}
+
+const generateNewPalette = () => {
+  event.preventDefault();
 }
