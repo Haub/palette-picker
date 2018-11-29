02a81b74754c48ef0267565fbe1fe46f3db1f52a
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-28 17:02:05 -0700

    Toggle locks is correctly toggling individual locks

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,14 @@
 
 const generateColorsButton = document.querySelector('.generate-colors');
-const lockButton = document.querySelector('.lock-button');
+const lockButton = document.querySelectorAll('.lock-button');
+// const colorParent = document.querySelector('.color');
+// const lockButton = document.getElementsByClassName('.lock-button');
 const colors = document.querySelectorAll('.color-button');
 const hexColors = document.querySelectorAll('h5');
 
 window.addEventListener('load', () => { createPalette() });
 generateColorsButton.addEventListener('click', () => { generateNewPalette() });
 
-lockButton.addEventListener('click', () => { toggleLock() })
 
 const createColor = () => {
   return '#' + ('000000' + Math.floor(16777216 * Math.random()).toString(16)).slice(-6);
@@ -17,7 +18,6 @@ const createColor = () => {
 const createPalette = () => {
   let newColorArray = [];
   const newColors = [...colors].forEach(color => {
-    // if(!)
     let newColor = createColor();
     color.style.backgroundColor = newColor;
     newColorArray.push(newColor);
@@ -29,8 +29,16 @@ const createPalette = () => {
 
 const generateNewPalette = () => {
   event.preventDefault();
+  createPalette();
 }
 
-const toggleLock = () => {
-  lockButton.classList.toggle("active")
+const toggleLock = (event) => {
+  let activeButton = event.target;
+  activeButton.classList.toggle("active")
 }
+
+lockButton.forEach(function(elem) {
+    elem.addEventListener("click", (event) => {
+    toggleLock(event)  
+    });
+});
