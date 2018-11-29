af0677238e0107df302188ba6445cb03670e8ee9
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-29 13:58:41 -0700

    Finish setting up knex environment in server.js and complete post to projects endpoint

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const app = express();
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Palette Picker';
 
-app.get('/', (response, request) => {
+app.get('/', (request, response) => {
   response.send('Welcome to Palette Picker')
 })
 
 app.get('/api/v1/projects', (request, response) => {
-
+ 
 });
 
-app.get('/api/v1/palettes', (request, response) => {
-  
+app.get('/api/v1/projects/:id', (request, response) => {
+
 });
 
 app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+  database('projects').insert(project, 'id')
+    .then((projectIds) => response.status(200).json({message: `New project with id of ${projectIds[0]} inserted successfully.`}))
+    .catch((error) => response.status(500).send({error: `Error: ${error.message}`}))
 });
 
-app.post('api/v1/palettes', (request, response) => {
+app.get('/api/v1/palettes', (request, response) => {
 
+})
+
+
+app.post('/api/v1/palettes', (request, response) => {
+  
 });
+
+app.delete('/api/v1/palettes', (request, response) => {
+
+})
+
 
 app.listen(3000, () => {
   console.log(`Palette Picker is running on ${app.get('port')}`);
