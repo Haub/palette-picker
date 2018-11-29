101ef9b10e8cf79592faf2f5746f6398e7b5175d
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-29 13:59:21 -0700

    Add knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/palettePicker',
+    connection: 'postgres://localhost/palettepicker',
     migrations: {
       directory: './db/migrations',
     },
@@ -14,36 +14,13 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+   production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL + `?ssl=true`',
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true
+  }
 
 };
